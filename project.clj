6658669305cfcf0e0f5a0d5da23c93f9b168ad2d
Toshiki Takeuchi <roimisia@gmail.com>
2016-04-29 21:49:12 +0900

    Release version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jungerer "0.1.0-SNAPSHOT"
+(defproject jungerer "0.1.0"
   :description "Clojure network/graph library wrapping JUNG"
   :url "https://github.com/totakke/jungerer"
   :license {:name "The BSD 3-Clause License"
