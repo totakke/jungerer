a2e5abda5852ee11cf9a33fe8b1e07d7d71959a4
Toshiki Takeuchi <roimisia@gmail.com>
2016-05-20 18:02:26 +0900

    Prepare for next development iteration (0.1.2-SNAPSHOT)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jungerer "0.1.1"
+(defproject jungerer "0.1.2-SNAPSHOT"
   :description "Clojure network/graph library wrapping JUNG"
   :url "https://github.com/totakke/jungerer"
   :license {:name "The BSD 3-Clause License"
