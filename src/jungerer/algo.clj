596bdd9a62db6717d1c35b02effdf9e7bd79b7f4
Toshiki Takeuchi <roimisia@gmail.com>
2016-06-23 15:24:03 +0900

    Write documents of some functions in jungerer.algo

--- src/jungerer/algo.clj
+++ src/jungerer/algo.clj
@@ -31,28 +31,46 @@
 ;; ------
 
 (defn barycenter-scorer
+  "Returns an edu.uci.ics.jung.algorithms.scoring.BarycenterScorer for graph. It
+  can be used for calculating scores for each node according to the sum of its
+  distances to all other nodes."
   [graph]
   (BarycenterScorer. graph))
 
 (defn degree-scorer
+  "Returns an edu.uci.ics.jung.algorithms.scoring.DegreeScorer for graph. It can
+  be used for calculating scores for each node equal to its degree."
   [graph]
   (DegreeScorer. graph))
 
 (defn betweenness-centrality
+  "Returns an edu.uci.ics.jung.algorithms.scoring.BetweennessCentrality for
+  graph. It can be used for calculating betweenness centrality for each node and
+  edge in the graph."
   [graph]
   (BetweennessCentrality. graph))
 
 (defn closeness-centrality
+  "Returns an edu.uci.ics.jung.algorithms.scoring.ClosenessCentrality for graph.
+  It can be used for calculating scores for each node based on the mean distance
+  to each other node."
   ([graph]
    (ClosenessCentrality. graph))
   ([^Hypergraph graph ^Distance distance]
    (ClosenessCentrality. graph distance)))
 
 (defn eigenvector-centrality
+  "Returns an edu.uci.ics.jung.algorithms.scoring.EigenvectorCentrality for
+  graph. It can be used for calculating eigenvector centrality for each node in
+  the graph. The 'eigenvector centrality' for a node is defined as the fraction
+  of time that a random walk(er) will spend at that node over an infinite time
+  horizon. Assumes that the graph is strongly connected."
   [graph]
   (EigenvectorCentrality. graph))
 
 (defn page-rank
+  "Returns an edu.uci.ics.jung.algorithms.scoring.PageRank for graph. It can be
+  used for calculating scores for each node according to the PageRank algorithm."
   ([graph]
    (page-rank graph 0.15))
   ([graph alpha]
@@ -60,6 +78,9 @@
      (.evaluate))))
 
 (defn hits
+  "Returns an edu.uci.ics.jung.algorithms.scoring.HITS for graph. It can be used
+  for calculating hub and authority scores to each node depending on the
+  topology of the network."
   [graph]
   (doto (HITS. graph)
     (.evaluate)))
@@ -67,5 +88,7 @@
 ;;
 
 (defn score
+  "Returns a score of node calculated with scorer algorithm. Note that type of
+  the returned score is different depending on the scorer."
   [^VertexScorer scorer node]
   (.getVertexScore scorer node))
